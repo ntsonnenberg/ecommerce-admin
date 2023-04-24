a84ceb7db25ea0a6c0b3ea160cf30d2c527a8e63
NTSonny <nsonnenberg@enlitehome.com>
2023-04-23 21:50:07 -0600

    Started implementing file upload for images of products. Not fully completed yet.

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
 	title: existingTitle,
 	description: existingDescription,
 	price: exisitngPrice,
+	images,
 }) {
 	const [title, setTitle] = useState(existingTitle || "");
 	const [description, setDescription] = useState(existingDescription || "");
@@ -34,6 +35,23 @@ export default function ProductForm({
 		router.push("/products");
 	}
 
+	const uploadImages = async (event) => {
+		const files = event.target?.files;
+
+		if (files?.length > 0) {
+			const data = new FormData();
+
+			for (const file of files) {
+				data.append("file", file);
+			}
+
+			const response = await axios.post("/api/upload", data, {
+				headers: { "Content-Type": "multipart/form-data" },
+			});
+			console.log(response.data);
+		}
+	};
+
 	return (
 		<form onSubmit={saveProduct}>
 			<label>Product Name</label>
@@ -43,6 +61,28 @@ export default function ProductForm({
 				value={title}
 				onChange={(event) => setTitle(event.target.value)}
 			/>
+			<label>Photos</label>
+			<div className="mb-2">
+				<label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-300">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+						/>
+					</svg>
+					<div>Upload</div>
+					<input type="file" onChange={uploadImages} className="hidden" />
+				</label>
+				{!images?.length && <div>No phots in this product </div>}
+			</div>
 			<label>Description</label>
 			<textarea
 				placeholder="description"
---
+++ pages/api/upload.js
@@ -0,0 +1,23 @@
+import multiparty from "multiparty";
+
+export default async function handle(req, res) {
+	const form = new multiparty.Form();
+
+	const { fields, files } = await new Promise((resolve, reject) => {
+		form.parse(req, async (error, fields, files) => {
+			if (error) {
+				reject(error);
+			}
+
+			resolve({ fields, files });
+		});
+	});
+
+	console.log("length: ", files.length);
+	console.log(fields);
+	res.json("ok");
+}
+
+export const config = {
+	api: { bodyParser: false },
+};
