3dbdb1eefa94a4a65b2b3e014ed11da4cb13c765
NTSonny <nsonnenberg@enlitehome.com>
2023-05-03 06:23:10 -0600

    Fully implemented update and delete of categories. Also implemented a parent category filed. Force installed react-sweetalert2 package which is using an outdated version of React. Might cause issues later on.

--- pages/categories.js
+++ pages/categories.js
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-export default function Categories() {
+function Categories({ swal }) {
+	const [editedCategory, setEditedCategory] = useState(null);
 	const [name, setName] = useState("");
 	const [categories, setCategories] = useState([]);
+	const [parentCategory, setParentCategory] = useState("");
 
 	useEffect(() => {
 		fetchCategories();
@@ -19,16 +22,56 @@ export default function Categories() {
 	const saveCategory = async (event) => {
 		event.preventDefault();
 
-		await axios.post("/api/categories", { name });
-		setName("");
+		const data = { name, parentCategory };
 
+		if (editedCategory) {
+			data._id = editedCategory._id;
+			await axios.put("/api/categories", data);
+
+			setEditedCategory(null);
+		} else {
+			await axios.post("/api/categories", data);
+		}
+
+		setName("");
 		fetchCategories();
+	};
+
+	const editCategory = (category) => {
+		setEditedCategory(category);
+		setName(category.name);
+		setParentCategory(category.parent?._id);
+	};
+
+	const deleteCategory = (category) => {
+		swal
+			.fire({
+				title: "Are you sure?",
+				text: `Do you want to delete ${category.name}`,
+				showCancelButton: true,
+				cancelButtonText: "Cancel",
+				confirmButtonText: "Yes, Delete!",
+				confirmButtonColor: "#d55",
+				reverseButtons: true,
+			})
+			.then(async (result) => {
+				if (result.isConfirmed) {
+					const { _id } = category;
+					await axios.delete("/api/categories?_id=" + _id);
+
+					fetchCategories();
+				}
+			});
 	};
 
 	return (
 		<Layout>
 			<h1>Categories</h1>
-			<label>New Category name</label>
+			<label>
+				{editedCategory
+					? `Edit Category ${editedCategory.name}`
+					: "Create New Category"}
+			</label>
 
 			<form onSubmit={saveCategory} className="flex gap-1">
 				<input
@@ -38,6 +81,17 @@ export default function Categories() {
 					value={name}
 					onChange={(event) => setName(event.target.value)}
 				/>
+				<select
+					className="mb-0"
+					onChange={(event) => setParentCategory(event.target.value)}
+					value={parentCategory}
+				>
+					<option value="">No parent category</option>
+					{categories.length > 0 &&
+						categories.map((category) => (
+							<option value={category._id}>{category.name}</option>
+						))}
+				</select>
 				<button type="submit" className="btn-primary">
 					Save
 				</button>
@@ -46,6 +100,8 @@ export default function Categories() {
 				<thead>
 					<tr>
 						<td>Category Name</td>
+						<td>Parent Category</td>
+						<td></td>
 					</tr>
 				</thead>
 				<tbody>
@@ -53,6 +109,21 @@ export default function Categories() {
 						categories.map((category) => (
 							<tr>
 								<td>{category.name}</td>
+								<td>{category?.parent?.name}</td>
+								<td>
+									<button
+										onClick={() => editCategory(category)}
+										className="btn-primary mr-1"
+									>
+										Edit
+									</button>
+									<button
+										onClick={() => deleteCategory(category)}
+										className="btn-primary"
+									>
+										Delete
+									</button>
+								</td>
 							</tr>
 						))}
 				</tbody>
@@ -60,3 +131,5 @@ export default function Categories() {
 		</Layout>
 	);
 }
+
+export default withSwal(({ swal }) => <Categories swal={swal} />);
