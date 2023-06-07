97e4d7be3844b1402a84ec044c25e6ad60bbfbe6
NTSonny <nsonnenberg@enlitehome.com>
2023-06-07 07:28:25 -0600

    Added column to orders to tell admin if each order was paid or not.

--- pages/orders.js
+++ pages/orders.js
@@ -18,6 +18,7 @@ export default function OrdersPage() {
 				<thead>
 					<tr>
 						<th>Date</th>
+						<th>Paid</th>
 						<th>Recipient</th>
 						<th>Products</th>
 					</tr>
@@ -27,6 +28,9 @@ export default function OrdersPage() {
 						orders.map((order) => (
 							<tr>
 								<td>{new Date(order.createdAt).toLocaleString()}</td>
+								<td className={order.paid ? "text-green-600" : "text-red-600"}>
+									{order.paid ? "YES" : "NO"}
+								</td>
 								<td>
 									{order.name} {order.email}
 									<br />
