f05bc5e76d89c942bc755635914931ec754ae19a
NTSonny <nsonnenberg@enlitehome.com>
2023-06-08 08:05:44 -0600

    Small bug fixed in file upload.

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -9,7 +9,7 @@ const bucketName = "nate-next-ecommerce";
 
 export default async function handle(req, res) {
 	await mongooseConnect();
-	await isAdminRequest(res, res);
+	await isAdminRequest(req, res);
 
 	const form = new multiparty.Form();
 
