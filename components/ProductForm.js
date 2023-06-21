871e6c3d7e90ac5d3c43825d205fb46ee5a84eca
NTSonny <nsonnenberg@enlitehome.com>
2023-06-20 20:09:50 -0600

    Image optimization using Image component.

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -3,215 +3,224 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Spinner from "./Spinner";
 import { ReactSortable } from "react-sortablejs";
+import Image from "next/image";
+import DefaultImage from "../public/default_product_image.png";
 
 export default function ProductForm({
-	_id,
-	title: existingTitle,
-	description: existingDescription,
-	price: exisitngPrice,
-	images: existingImages,
-	category: assignedCategory,
-	properties: assignedProperties,
+  _id,
+  title: existingTitle,
+  description: existingDescription,
+  price: exisitngPrice,
+  images: existingImages,
+  category: assignedCategory,
+  properties: assignedProperties,
 }) {
-	const [title, setTitle] = useState(existingTitle || "");
-	const [description, setDescription] = useState(existingDescription || "");
-	const [category, setCategory] = useState(assignedCategory || "");
-	const [productProperties, setProductProperties] = useState(
-		assignedProperties || {}
-	);
-	const [price, setPrice] = useState(exisitngPrice || "");
-	const [images, setImages] = useState(existingImages || []);
-	const [goToProducts, setGoToProducts] = useState(false);
-	const [isUploading, setIsUploading] = useState(false);
-	const [categories, setCategories] = useState([]);
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [category, setCategory] = useState(assignedCategory || "");
+  const [productProperties, setProductProperties] = useState(
+    assignedProperties || {}
+  );
+  const [price, setPrice] = useState(exisitngPrice || "");
+  const [images, setImages] = useState(existingImages || []);
+  const [goToProducts, setGoToProducts] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const router = useRouter();
 
-	const router = useRouter();
+  useEffect(() => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }, []);
 
-	useEffect(() => {
-		axios.get("/api/categories").then((result) => {
-			setCategories(result.data);
-		});
-	}, []);
+  const saveProduct = async (event) => {
+    event.preventDefault();
 
-	const saveProduct = async (event) => {
-		event.preventDefault();
+    const data = {
+      title,
+      description,
+      price,
+      images,
+      category,
+      properties: productProperties,
+    };
 
-		const data = {
-			title,
-			description,
-			price,
-			images,
-			category,
-			properties: productProperties,
-		};
+    if (_id) {
+      //update
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      //create
+      await axios.post("/api/products", data);
+    }
 
-		if (_id) {
-			//update
-			await axios.put("/api/products", { ...data, _id });
-		} else {
-			//create
-			await axios.post("/api/products", data);
-		}
+    setGoToProducts(true);
+  };
 
-		setGoToProducts(true);
-	};
+  if (goToProducts) {
+    router.push("/products");
+  }
 
-	if (goToProducts) {
-		router.push("/products");
-	}
+  const uploadImages = async (event) => {
+    const files = event.target?.files;
 
-	const uploadImages = async (event) => {
-		const files = event.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
 
-		if (files?.length > 0) {
-			setIsUploading(true);
+      const data = new FormData();
 
-			const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
 
-			for (const file of files) {
-				data.append("file", file);
-			}
+      const response = await axios.post("/api/upload", data);
 
-			const response = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...response.data.links];
+      });
 
-			setImages((oldImages) => {
-				return [...oldImages, ...response.data.links];
-			});
+      setIsUploading(false);
+    }
+  };
 
-			setIsUploading(false);
-		}
-	};
+  const updateImagesOrder = (images) => {
+    setImages(images);
+  };
 
-	const updateImagesOrder = (images) => {
-		setImages(images);
-	};
+  const setProductProp = (propName, value) => {
+    setProductProperties((prev) => {
+      const newProductProps = { ...prev };
+      newProductProps[propName] = value;
 
-	const setProductProp = (propName, value) => {
-		setProductProperties((prev) => {
-			const newProductProps = { ...prev };
-			newProductProps[propName] = value;
+      return newProductProps;
+    });
+  };
 
-			return newProductProps;
-		});
-	};
+  const propertiesToFill = [];
+  if (categories.length > 0 && category) {
+    let catInfo = categories.find(({ _id }) => _id === category);
 
-	const propertiesToFill = [];
-	if (categories.length > 0 && category) {
-		let catInfo = categories.find(({ _id }) => _id === category);
+    propertiesToFill.push(...catInfo.properties);
 
-		propertiesToFill.push(...catInfo.properties);
+    while (catInfo?.parent?._id) {
+      const parentCat = categories.find(
+        ({ _id }) => _id === catInfo?.parent?._id
+      );
 
-		while (catInfo?.parent?._id) {
-			const parentCat = categories.find(
-				({ _id }) => _id === catInfo?.parent?._id
-			);
+      propertiesToFill.push(...parentCat.properties);
+      catInfo = parentCat;
+    }
+  }
 
-			propertiesToFill.push(...parentCat.properties);
-			catInfo = parentCat;
-		}
-	}
+  // console.log("Images:", images);
 
-	return (
-		<form onSubmit={saveProduct}>
-			<label>Product Name</label>
-			<input
-				type="text"
-				placeholder="product name"
-				value={title}
-				onChange={(event) => setTitle(event.target.value)}
-			/>
-			<label>Category</label>
-			<select
-				value={category}
-				onChange={(event) => setCategory(event.target.value)}
-			>
-				<option value="">Uncategorized</option>
-				{categories.length > 0 &&
-					categories.map((category) => (
-						<option value={category._id} key={category._id}>
-							{category.name}
-						</option>
-					))}
-			</select>
-			{propertiesToFill.length > 0 &&
-				propertiesToFill.map((property) => (
-					<div key={property._id}>
-						<label>
-							{property.name[0].toUpperCase() + property.name.substring(1)}
-						</label>
-						<div>
-							<select
-								value={productProperties[property.name]}
-								onChange={(event) =>
-									setProductProp(property.name, event.target.value)
-								}
-							>
-								{property.values.map((value) => (
-									<option value={value} key={value}>
-										{value}
-									</option>
-								))}
-							</select>
-						</div>
-					</div>
-				))}
-			<label>Photos</label>
-			<div className="mb-2 flex flex-wrap gap-1">
-				<ReactSortable
-					list={images}
-					className="flex flex-wrap gap-1"
-					setList={updateImagesOrder}
-				>
-					{!!images?.length &&
-						images.map((link) => (
-							<div
-								key={link}
-								className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200"
-							>
-								<img src={link} alt="" className="rounded-lg" />
-							</div>
-						))}
-				</ReactSortable>
-				{isUploading && (
-					<div className="h-24 flex items-center">
-						<Spinner />
-					</div>
-				)}
-				<label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						className="w-6 h-6"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-						/>
-					</svg>
-					<div>Add image</div>
-					<input type="file" onChange={uploadImages} className="hidden" />
-				</label>
-			</div>
-			<label>Description</label>
-			<textarea
-				placeholder="description"
-				value={description}
-				onChange={(event) => setDescription(event.target.value)}
-			/>
-			<label>Price (in USD)</label>
-			<input
-				type="number"
-				placeholder="price"
-				value={price}
-				onChange={(event) => setPrice(event.target.value)}
-			/>
-			<button type="submit" className="btn-primary">
-				Save
-			</button>
-		</form>
-	);
+  return (
+    <form onSubmit={saveProduct}>
+      <label>Product Name</label>
+      <input
+        type="text"
+        placeholder="product name"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <label>Category</label>
+      <select
+        value={category}
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value="">Uncategorized</option>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <option value={category._id} key={category._id}>
+              {category.name}
+            </option>
+          ))}
+      </select>
+      {propertiesToFill.length > 0 &&
+        propertiesToFill.map((property) => (
+          <div key={property.name}>
+            <label>
+              {property.name[0].toUpperCase() + property.name.substring(1)}
+            </label>
+            <div>
+              <select
+                value={productProperties[property.name]}
+                onChange={(event) =>
+                  setProductProp(property.name, event.target.value)
+                }
+              >
+                {property.values.map((value) => (
+                  <option value={value} key={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        ))}
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-1">
+        <ReactSortable
+          list={images}
+          className="flex flex-wrap gap-1"
+          setList={updateImagesOrder}
+        >
+          {!!images?.length &&
+            images.map((link) => (
+              <div
+                key={link}
+                className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200"
+              >
+                <Image
+                  src={link || DefaultImage}
+                  alt=""
+                  className="rounded-lg"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+        </ReactSortable>
+        {isUploading && (
+          <div className="h-24 flex items-center">
+            <Spinner />
+          </div>
+        )}
+        <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Add image</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+      </div>
+      <label>Description</label>
+      <textarea
+        placeholder="description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <label>Price (in USD)</label>
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+      <button type="submit" className="btn-primary">
+        Save
+      </button>
+    </form>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/AGNmyxYfyy5xNAIBBCUNWklj11x2pF3Ic_Sp0pcVAAdYCQ=s96-c",
+      },
+      {
+        protocol: "https",
+        hostname: "nate-next-ecommerce.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
